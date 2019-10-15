3ad90822854401771407bbd13ca9b1037322cfa6
Haider Raza <sagihaider@gmail.com>
2019-10-15 23:00:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ from keras.layers import Conv2D, MaxPool2D
 
 from tqdm import tqdm_notebook,trange
 import matplotlib.pyplot as plt
-
+import gc #garbage collection, we need to save all the RAM we can
 #%matplotlib inline
 
 #%%
@@ -39,7 +39,211 @@ train_path = path + 'train/'
 test_path = path + 'test/'
 
 df = pd.DataFrame({'path': glob(os.path.join(train_path,'*.tif'))}) # load the filenames
-df['id'] = df.path.map(lambda x: x.split('/')[3].split(".")[0]) # keep only the file names in 'id'
+df['id'] = df.path.map(lambda x: x.split('/')[6].split(".")[0]) # keep only the file names in 'id' because after 6 slash the file name is there
 labels = pd.read_csv(path+"train_labels.csv") # read the provided labels
 df = df.merge(labels, on = "id") # merge labels and filepaths
 df.head(3) # print the first three entrys
+
+
+#%% Load the images
+
+# Now, we will load some of the images. As interactive kernels with GPU currently offer 
+# about 14 GB of RAM, we will take care to keep the images in the uint8 format 
+# (i.e. pixel values are integers between 0 and 255) to reduce the memory footprint.
+# Processing of the images often requires converting them to float32, which would
+# require additional space. We'll declare a function to load a set number of images 
+# and then load 10000 images.
+
+
+
+def load_data(N,df):
+    """ This functions loads N images using the data df
+    """
+    # allocate a numpy array for the images (N, 96x96px, 3 channels, values 0 - 255)
+    X = np.zeros([N,96,96,3],dtype=np.uint8) 
+    #convert the labels to a numpy array too
+    y = np.squeeze(df.as_matrix(columns=['label']))[0:N]
+    #read images one by one, tdqm notebook displays a progress bar
+    for i, row in tqdm_notebook(df.iterrows(), total=N):
+        if i == N:
+            break
+        X[i] = cv2.imread(row['path'])
+          
+    return X,y
+
+
+# Load 10k images
+N=1000
+X,y = load_data(N=N,df=df) 
+
+#%%
+fig = plt.figure(figsize=(10, 4), dpi=150)
+np.random.seed(100) #we can use the seed to get a different set of random images
+for plotNr,idx in enumerate(np.random.randint(0,N,8)):
+    ax = fig.add_subplot(2, 8//2, plotNr+1, xticks=[], yticks=[]) #add subplots
+    plt.imshow(X[idx]) #plot image
+    ax.set_title('Label: ' + str(y[idx])) #show the label corresponding to the image
+    
+
+fig = plt.figure(figsize=(4, 2),dpi=150)
+plt.bar([1,0], [(y==0).sum(), (y==1).sum()]); #plot a bar chart of the label frequency
+plt.xticks([1,0],["Negative (N={})".format((y==0).sum()),"Positive (N={})".format((y==1).sum())]);
+plt.ylabel("# of samples")
+           
+positive_samples = X[y == 1]
+negative_samples = X[y == 0]
+
+#%% 
+nr_of_bins = 256 #each possible pixel value will get a bin in the following histograms
+fig,axs = plt.subplots(4,2,sharey=True,figsize=(8,8),dpi=150)
+
+#RGB channels
+axs[0,0].hist(positive_samples[:,:,:,0].flatten(),bins=nr_of_bins,density=True)
+axs[0,1].hist(negative_samples[:,:,:,0].flatten(),bins=nr_of_bins,density=True)
+axs[1,0].hist(positive_samples[:,:,:,1].flatten(),bins=nr_of_bins,density=True)
+axs[1,1].hist(negative_samples[:,:,:,1].flatten(),bins=nr_of_bins,density=True)
+axs[2,0].hist(positive_samples[:,:,:,2].flatten(),bins=nr_of_bins,density=True)
+axs[2,1].hist(negative_samples[:,:,:,2].flatten(),bins=nr_of_bins,density=True)
+
+#All channels
+axs[3,0].hist(positive_samples.flatten(),bins=nr_of_bins,density=True)
+axs[3,1].hist(negative_samples.flatten(),bins=nr_of_bins,density=True)
+
+#Set image labels
+axs[0,0].set_title("Positive samples (N =" + str(positive_samples.shape[0]) + ")");
+axs[0,1].set_title("Negative samples (N =" + str(negative_samples.shape[0]) + ")");
+axs[0,1].set_ylabel("Red",rotation='horizontal',labelpad=35,fontsize=12)
+axs[1,1].set_ylabel("Green",rotation='horizontal',labelpad=35,fontsize=12)
+axs[2,1].set_ylabel("Blue",rotation='horizontal',labelpad=35,fontsize=12)
+axs[3,1].set_ylabel("RGB",rotation='horizontal',labelpad=35,fontsize=12)
+for i in range(4):
+    axs[i,0].set_ylabel("Relative frequency")
+axs[3,0].set_xlabel("Pixel value")
+axs[3,1].set_xlabel("Pixel value")
+fig.tight_layout()
+
+#%%
+
+nr_of_bins = 64 #we use a bit fewer bins to get a smoother image
+fig,axs = plt.subplots(1,2,sharey=True, sharex = True, figsize=(8,2),dpi=150)
+axs[0].hist(np.mean(positive_samples,axis=(1,2,3)),bins=nr_of_bins,density=True);
+axs[1].hist(np.mean(negative_samples,axis=(1,2,3)),bins=nr_of_bins,density=True);
+axs[0].set_title("Mean brightness, positive samples");
+axs[1].set_title("Mean brightness, negative samples");
+axs[0].set_xlabel("Image mean brightness")
+axs[1].set_xlabel("Image mean brightness")
+axs[0].set_ylabel("Relative frequency")
+axs[1].set_ylabel("Relative frequency");
+
+# %%
+N = df["path"].size # get the number of images in the training data set
+X,y = load_data(N=N,df=df)
+
+#%% 
+
+#Collect garbage
+positives_samples = None
+negative_samples = None
+gc.collect();
+
+training_portion = 0.8 # Specify training/validation ratio
+split_idx = int(np.round(training_portion * y.shape[0])) #Compute split idx
+
+np.random.seed(42) #set the seed to ensure reproducibility
+
+#shuffle
+idx = np.arange(y.shape[0])
+np.random.shuffle(idx)
+X = X[idx]
+y = y[idx]
+
+#%% Define Network
+
+#just some network parameters, see above link regarding the layers for details
+kernel_size = (3,3)
+pool_size= (2,2)
+first_filters = 32
+second_filters = 64
+third_filters = 128
+
+#dropout is used for regularization here with a probability of 0.3 for conv layers, 0.5 for the dense layer at the end
+dropout_conv = 0.3
+dropout_dense = 0.5
+
+#initialize the model
+model = Sequential()
+
+#now add layers to it
+
+#conv block 1
+model.add(Conv2D(first_filters, kernel_size, input_shape = (96, 96, 3)))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(Conv2D(first_filters, kernel_size, use_bias=False))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(MaxPool2D(pool_size = pool_size)) 
+model.add(Dropout(dropout_conv))
+
+#conv block 2
+model.add(Conv2D(second_filters, kernel_size, use_bias=False))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(Conv2D(second_filters, kernel_size, use_bias=False))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(MaxPool2D(pool_size = pool_size))
+model.add(Dropout(dropout_conv))
+
+#conv block 3
+model.add(Conv2D(third_filters, kernel_size, use_bias=False))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(Conv2D(third_filters, kernel_size, use_bias=False))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(MaxPool2D(pool_size = pool_size))
+model.add(Dropout(dropout_conv))
+
+#a fully connected (also called dense) layer at the end
+model.add(Flatten())
+model.add(Dense(256, use_bias=False))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(Dropout(dropout_dense))
+
+#finally convert to values of 0 to 1 using the sigmoid activation function
+model.add(Dense(1, activation = "sigmoid"))
+
+model.summary()
+
+#%% Compile Model
+
+batch_size = 50
+
+model.compile(loss=keras.losses.binary_crossentropy,
+              optimizer=keras.optimizers.Adam(0.001), 
+              metrics=['accuracy'])
+
+#%% 
+
+#normally you would want to reshuffle the data between epochs, we don't as we split in-place into training/validation
+epochs = 3 #how many epochs we want to perform
+for epoch in range(epochs):
+    #compute how many batches we'll need
+    iterations = np.floor(split_idx / batch_size).astype(int) #the floor makes us discard a few samples here, I got lazy...
+    loss,acc = 0,0 #we will compute running loss and accuracy
+    with trange(iterations) as t: #display a progress bar
+        for i in t:
+            start_idx = i * batch_size #starting index of the current batch
+            x_batch = X[start_idx:start_idx+batch_size] #the current batch
+            y_batch = y[start_idx:start_idx+batch_size] #the labels for the current batch
+
+            metrics = model.train_on_batch(x_batch, y_batch) #train the model on a batch
+
+            loss = loss + metrics[0] #compute running loss
+            acc = acc + metrics[1] #compute running accuracy
+            t.set_description('Running training epoch ' + str(epoch)) #set progressbar title
+            t.set_postfix(loss="%.2f" % round(loss / (i+1),2),acc="%.2f" % round(acc / (i+1),2)) #display metrics
+            
+#%% 
